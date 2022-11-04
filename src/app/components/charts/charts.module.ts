375f79f29911b7d4685b3275da406b7efea423bc
Imroz <imrozkhn7@gmail.com>
2022-11-04 16:10:20 -0400

    Adding highcharts

--- src/app/components/charts/charts.module.ts
+++ src/app/components/charts/charts.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ChartsComponent } from './charts.component';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
-import { PieChartComponent } from './pie-chart/pie-chart.component';
-import { LineChartComponent } from './line-chart/line-chart.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ChartsComponent} from './charts.component';
+import {BarChartComponent} from './bar-chart/bar-chart.component';
+import {PieChartComponent} from './pie-chart/pie-chart.component';
+import {LineChartComponent} from './line-chart/line-chart.component';
 import {NgxChartsModule} from "@swimlane/ngx-charts";
-
+import {ChartModule} from "angular-highcharts";
+import { BulletChartComponent } from './bullet-chart/bullet-chart.component';
 
 
 @NgModule({
@@ -13,14 +14,17 @@ import {NgxChartsModule} from "@swimlane/ngx-charts";
     ChartsComponent,
     BarChartComponent,
     PieChartComponent,
-    LineChartComponent
+    LineChartComponent,
+    BulletChartComponent
   ],
   exports: [
     ChartsComponent
   ],
   imports: [
     CommonModule,
-    NgxChartsModule
+    NgxChartsModule,
+    ChartModule
   ]
 })
-export class ChartsModule { }
+export class ChartsModule {
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GridWrapperComponent } from './grid-wrapper/grid-wrapper.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GridWrapperComponent} from './grid-wrapper/grid-wrapper.component';
 import {GridsterModule} from "angular-gridster2";
 import {ChartsModule} from "./charts/charts.module";
-import { DashboardWrapperComponent } from './dashboard-wrapper/dashboard-wrapper.component';
+import {DashboardWrapperComponent} from './dashboard-wrapper/dashboard-wrapper.component';
 import {TabMenuModule} from "primeng/tabmenu";
-import { ProfileComponent } from './profile/profile.component';
-import { TableComponent } from './table/table.component';
-import { ParagraphComponent } from './paragraph/paragraph.component';
-import { CountsComponent } from './counts/counts.component';
-import { VideoCardComponent } from './video-card/video-card.component';
+import {ProfileComponent} from './profile/profile.component';
+import {TableComponent} from './table/table.component';
+import {ParagraphComponent} from './paragraph/paragraph.component';
+import {CountsComponent} from './counts/counts.component';
+import {VideoCardComponent} from './video-card/video-card.component';
 import {TableModule} from "primeng/table";
 import {PipesModule} from "../pipes/pipes.module";
 import {ProgressSpinnerModule} from "primeng/progressspinner";
-
 
 
 @NgModule({
@@ -26,18 +25,19 @@ import {ProgressSpinnerModule} from "primeng/progressspinner";
     CountsComponent,
     VideoCardComponent
   ],
-    exports: [
-        GridWrapperComponent,
-        DashboardWrapperComponent
-    ],
-    imports: [
-        CommonModule,
-        GridsterModule,
-        ChartsModule,
-        TabMenuModule,
-        TableModule,
-        PipesModule,
-        ProgressSpinnerModule
-    ]
+  exports: [
+    GridWrapperComponent,
+    DashboardWrapperComponent
+  ],
+  imports: [
+    CommonModule,
+    GridsterModule,
+    ChartsModule,
+    TabMenuModule,
+    TableModule,
+    PipesModule,
+    ProgressSpinnerModule
+  ]
 })
-export class ComponentsModule { }
+export class ComponentsModule {
+}
